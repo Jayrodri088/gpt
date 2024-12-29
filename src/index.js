7a8094b4b1f7edc229db415fc377a0cf845b47b1
Jayrodri088 <johnbetho.c.088@gmail.com>
2024-12-29 09:40:30 +0100

    add initial component and style files for the application layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import reactDom from 'react-dom';
-
+import ReactDOM from 'react-dom/client'; // Import createRoot from react-dom/client
 import App from './App';
+import './index.css'
 
+// Get the root element from the DOM
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// Use ReactDOM.createRoot to create a root
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the App component
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
